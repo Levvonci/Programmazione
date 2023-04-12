e5dd948c0d1097b081d6dbe6d2766d11f4acc220
Levvonci <lvnci@tutanota.com>
2023-04-12 15:41:34 +0200

    cycles

--- python/cycles.py
+++ python/cycles.py
@@ -1,1 +1,28 @@
+scatola = ['mela', 'pera', 'banana']
+for numeri in scatola:
+    print(f"{numeri.title()}, sono muffa")
+    print(f"che frutto sei?, {numeri.title()}\n")
+
+print('grazie a tutti i frutti per essere stati qui')
+#primo for
+
+'''Esercizio 4.1/4.2 (Page 56)'''
+
+pizze = ['margherite', 'funghi', 'patate']
+
+for pizza in pizze:
+    print(f"i like {pizza.title()}")
+
+print('i rly like pizze margherita bc its')
+
+print('qui finisce il primo esercizio')
+print('qui inizia il secondo esercizio')
+
+'''4.2'''
+
+animali = ['cane', 'gatto', 'topo']
+for animale in animali:
+    print(animale)
+    print(f"The {animale.title()}, is absolutly cute")
+print('This pets are absolutly cute')
 
