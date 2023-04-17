77a619411d64f808182d5d7974076d2890ef7e7a
Levvonci <lvnci@tutanota.com>
2023-04-17 08:24:48 +0200

    17/04/23

--- python/if.py
+++ python/if.py
@@ -15,3 +15,32 @@ macchina = 'audi'
 if macchina != 'tesla':
     print('fa cagare')
 
+'''verifica elemento in lista'''
+
+lista = ['mamma', 'papà', 'figlio']
+
+if 'mamma'in lista:
+    print('mamma è nella lista')
+else:
+    print('mamma non è nella lista')
+
+''''''
+banned_user = ['martina', 'nicolò', 'franco', 'damiano']
+user = 'davide'
+
+if user not in banned_user:
+    print(f"{user.title()}, you are not banned, lets continue you    r game")
+else:
+    print(f"{user.title()}, you are banned, stop playing right no    w")
+
+
+'''esercizio random'''
+
+lista1 = ['audi','tesla','bmw','subaru']
+lista2 = ['marco','luca','gianni','mario']
+lista3 = []
+
+for macchina in lista1 && persona in lista2:
+    print('sono dentro il for')
+
+           
